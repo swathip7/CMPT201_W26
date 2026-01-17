b876c7659bff42059f299af29f885d05442b95c5
swathip7 <premalals@mymacewan.ca>
2026-01-17 04:53:17 +0000

    message

---
+++ Labs/Lab1/hello.c
@@ -0,0 +1,14 @@
+/*
+ID: 3147620
+Name: Swathi Prema Lal
+Lab Instuctor: Dhara Wagh
+Professor: Ardy Rd
+Lab Section: AS01
+*/
+
+#include<stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
--- Labs/Lab1/main.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("Hello World!\n");
-    printf("Welcome to CMPT201 lab\n");
-    return 0;
-}
